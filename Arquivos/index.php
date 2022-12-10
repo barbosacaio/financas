1fa3818f6d5ac19b7ba224978d78f0b2321dd6e1
barbosacaio <caiiohenrique2004@gmail.com>
2022-12-10 17:14:03 -0300

    v1.2

--- Arquivos/index.php
+++ Arquivos/index.php
@@ -6,13 +6,13 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 
     <!-- Logo do Website -->
-    <link rel='shortcut icon' sizes='196x196' href='Imagens/Logos/financas_lightmode_64px.png'>
+    <link rel='shortcut icon' sizes='196x196' href='Imagens/Logos/financas_claro_64px.png'>
 
     <!-- Importando Bootstrap (CSS e JS) -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
+    <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
+    <script src='https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
 
     <!-- Arquivo CSS -->
     <link rel='stylesheet' type='text/css' href='style.css'>
@@ -21,16 +21,19 @@
     <script type='text/javascript' src='script.js'></script>
 
     <!-- Importando FontAwesome -->
-    <script src="https://kit.fontawesome.com/ea7035a244.js" crossorigin="anonymous"></script>
+    <script src='https://kit.fontawesome.com/ea7035a244.js' crossorigin='anonymous'></script>
+
+    <!-- Importando Typed.js -->
+    <script src='https://cdn.jsdelivr.net/npm/typed.js@2.0.12'></script>
 
     <title>Finanças | Home</title>
 </head>
-<body class='modo-escuro'>
+<body class='modo-escuro' onload='autoType(), configurarGrid()'>
     <!-- Navbar -->
-    <nav class='navbar navbar-expand-md py-4' id='navbar'>
+    <nav class='navbar navbar-expand-md py-4 sticky-top' id='navbar'>
         <!-- Logo e nome do site -->
         <!-- O nome some em telas menores que 'lg' -->
-        <a class='navbar-brand' href='index.php'><img src='Imagens/Logos/financas_darkmode_64px.png' id='logoFinancas' height='30' alt='Ícone do Finanças'><p class='d-none d-lg-inline ml-3 mr-5'>Finanças</p></a>
+        <a class='navbar-brand' href='index.php'><img src='Imagens/Logos/financas_escuro_64px.png' id='logoFinancas' height='30' alt='Ícone do Finanças'><p class='d-none d-lg-inline ml-3 mr-5'>Finanças</p></a>
         <!-- Botão de dropdown para dispositivos pequenos -->
         <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#linksNavbar' aria-controls='linksNavbar' aria-expanded='false' aria-label='Alterar Navbar'>
             <img src='Imagens/Utilitários/hamburguer_escuro_64px.png' alt='Menu Hambúrguer' height='25' id='hamburguer'>
@@ -40,7 +43,7 @@
         <div class='collapse navbar-collapse' id='linksNavbar'>
             <ul class='navbar-nav mr-auto'>
                 <li class='nav-item'>
-                    <a class='nav-link mx-1' href='index.php#home'>Home</a>
+                    <a class='nav-link mx-1' href='index.php'>Home</a>
                 </li>
                 <li class='nav-item'>
                     <a class='nav-link mx-1' href='index.php#funcoes'>Funções</a>
@@ -55,7 +58,8 @@
             <!-- //Links da navbar -->
             <!-- Botões da navbar -->
             <div class='container-fluid pl-0'>
-                <button class='btn float-right' id='botaoAlterarModo' type='button' onclick='alterarModo()'><img id='alterarModoIcone' src='Imagens/Utilitários/alterarModo_escuro_64px.png' height='30' alt='Ícone para alterar modo de cor'></button>
+                <button class='btn float-right' id='botaoAlterarModo' type='button' onclick='alterarModo(), navbarVisivel()'><img id='alterarModoIcone' src='Imagens/Utilitários/alterarModo_escuro_64px.png' height='45' alt='Ícone para alterar modo de cor' class='pb-3'></button>
+                <button class='btn float-right' id='botaoNotificacoes' type='button'><img id='abrirNotificacoes' src='Imagens/Utilitários/sino_escuro_64px.png' height='15' alt='Ícone de notificações'></button>
                 <button class='btn float-right mr-2' id='botaoCadastrar' type='button'>Cadastrar</button>
                 <button class='btn float-right mr-2' id='botaoEntrar' type='button'>Entrar</button>
             </div>
@@ -63,5 +67,74 @@
         </div>
     </nav>
     <!-- //Navbar -->
+    <!-- Home -->
+    <main id='home'>
+        <div class='container-fluid' id='slogan'>
+            <!-- Grid que divide a tela em duas metades -->
+            <div class="row">
+                <!-- Metade da esquerda -->
+                <div class="col-sm w-50 pl-5 pt-5" id='sloganContainer'>
+                    <!-- Slogan concatenado com o AutoType -->
+                    <h1>Seu website de <span class='auto-type'></span></h1>
+                    <!-- Texto explicativo -->
+                    <h5 class='mt-5'>Finanças ajuda o gerenciamento financeiro e a visão geral das suas receitas e despesas, 
+                        disponibilizando também a área de gestão onde há gráficos que permitem maior visualização do próprio 
+                        usuário.</h5>
+                    <!-- Botões para encaminhar o usuário -->
+                    <a href='index.php#funcoes'><button class='btn mt-4 mb-4 p-2' id='botaoSaberMais' type='button'>Saber mais <i class="fa-solid fa-arrow-right ml-2"></i></button></a>
+                    <a href='index.php#historia'><button class='btn mt-4 mb-4 ml-2 p-2' id='botaoHistoria' type='button'>Quem somos?</button></a>
+                </div>
+                <!-- Metade da direita -->
+                <div class="col-sm w-50" id='imagemContainer'>
+                    <!-- Imagem de decoração -->
+                    <img src='Imagens/Design/desenho_notebook.png' alt='Desenho de um Notebook' width='90%' height='80%'>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- //Home -->
+    <!-- Funções -->
+    <section id='funcoes'>
+        <h1 class='tituloArea'>As principais funções</h1>
+        <p class='subtituloArea'>Todas as funções foram pensadas para suprir problemas atuais em aplicativos de gestão financeira,
+            tornando o mais intuitivo possível para facilitar o uso por qualquer pessoa, além de evitar perda de informação ao trocar
+            de dispositivo.
+        </p>
+        <div class="row">
+            <div class="col-sm w-33">
+                <img src='Imagens/Utilitários/receitas_escuro_64px.png' alt='Símbolo Receitas' id='simboloReceitas'>
+                <h3 class='tituloFuncao'>Receitas</h3>
+                <p class='subtituloFuncao'>Controle sobre o valor total e origem das suas receitas</p>
+            </div>
+            <div class="col-sm w-33">
+                <img src='Imagens/Utilitários/despesas_escuro_64px.png' alt='Símbolo Despesas' id='simboloDespesas'>
+                <h3 class='tituloFuncao'>Despesas</h3>
+                <p class='subtituloFuncao'>Possibilidade de comparar as fontes de suas despesas
+            </div>
+            <div class="col-sm w-33">
+                <img src='Imagens/Utilitários/usuarios_escuro_64px.png' alt='Símbolo Usuários' id='simboloUsuarios'>
+                <h3 class='tituloFuncao'>Conta Pessoal</h3>
+                <p class='subtituloFuncao'>Conta pessoal e privada para salvar suas informações em qualquer dispositivo que você utilize</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm w-33">
+                <img src='Imagens/Utilitários/seguranca_escuro_64px.png' alt='Símbolo Segurança' id='simboloSeguranca'>
+                <h3 class='tituloFuncao'>Proteção de dados</h3>
+                <p class='subtituloFuncao'>Todos os dados disponibilizados pelos usuários são guardados e nunca divulgados. Toda senha é criptografada até mesmo para nós</p>
+            </div>
+            <div class="col-sm w-33">
+                <img src='Imagens/Utilitários/sino_escuro_64px.png' alt='Símbolo Notificações' id='simboloNotificacoes'>
+                <h3 class='tituloFuncao'>Sistema de notificações</h3>
+                <p class='subtituloFuncao'>Toda atualização no modo de uso ou na conta dos usuários é notificada através do ícone na barra de navegação</p>
+            </div>
+            <div class="col-sm w-33">
+                <img src='Imagens/Utilitários/pdf_escuro_64px.png' alt='Símbolo PDF' id='simboloPDF'>
+                <h3 class='tituloFuncao'>Impressão de informação</h3>
+                <p class='subtituloFuncao'>Você pode gerar um arquivo PDF com todos os dados da sua conta durante o mês para ser impresso</p>
+            </div>
+        </div>
+    </section>
+    <!-- //Funções -->
 </body>
 </html>
--- Arquivos/script.js
+++ Arquivos/script.js
@@ -1,3 +1,7 @@
+// Chama funções necessárias
+window.addEventListener('resize', configurarGrid);
+window.addEventListener('scroll', navbarVisivel);
+
 // Função para alterar entre modo claro e escuro
 function alterarModo(){
     // Seleciona o 'body' do 'index.php'
@@ -6,18 +10,92 @@ function alterarModo(){
     elemento.classList.toggle('modo-escuro');
     // Salvando elementos que precisam ser alterados em variáveis
     var alterarModoIcone = document.getElementById('alterarModoIcone');
+    var notificacoes = document.getElementById('abrirNotificacoes');
     var logo = document.getElementById('logoFinancas');
     var hamburguer = document.getElementById('hamburguer');
+    var simboloReceitas = document.getElementById('simboloReceitas');
+    var simboloDespesas = document.getElementById('simboloDespesas');
+    var simboloUsuarios = document.getElementById('simboloUsuarios');
+    var simboloSeguranca = document.getElementById('simboloSeguranca');
+    var simboloNotificacoes = document.getElementById('simboloNotificacoes');
+    var simboloPDF = document.getElementById('simboloPDF');
     // Verifica se o 'body' está ou não com a classe
     if(elemento.classList.contains('modo-escuro')){
-        // Altera o ícone para o de modo escuro
+        // Altera itens necessários para o modo escuro
         jQuery(alterarModoIcone).attr('src', 'Imagens/Utilitários/alterarModo_escuro_64px.png');
-        jQuery(logo).attr('src', 'Imagens/Logos/financas_darkmode_64px.png');
+        jQuery(notificacoes).attr('src', 'Imagens/Utilitários/sino_escuro_64px.png');
+        jQuery(logo).attr('src', 'Imagens/Logos/financas_escuro_64px.png');
         jQuery(hamburguer).attr('src', 'Imagens/Utilitários/hamburguer_escuro_64px.png');
+        jQuery(simboloReceitas).attr('src', 'Imagens/Utilitários/receitas_escuro_64px.png');
+        jQuery(simboloDespesas).attr('src', 'Imagens/Utilitários/despesas_escuro_64px.png');
+        jQuery(simboloUsuarios).attr('src', 'Imagens/Utilitários/usuarios_escuro_64px.png');
+        jQuery(simboloSeguranca).attr('src', 'Imagens/Utilitários/seguranca_escuro_64px.png');
+        jQuery(simboloNotificacoes).attr('src', 'Imagens/Utilitários/sino_escuro_64px.png');
+        jQuery(simboloPDF).attr('src', 'Imagens/Utilitários/pdf_escuro_64px.png');
     }else{
-        // Altera o ícone para o de modo claro
+        // Altera itens necessários para o modo claro
         jQuery(alterarModoIcone).attr('src', 'Imagens/Utilitários/alterarModo_claro_64px.png');
-        jQuery(logo).attr('src', 'Imagens/Logos/financas_lightmode_64px.png');
+        jQuery(notificacoes).attr('src', 'Imagens/Utilitários/sino_claro_64px.png');
+        jQuery(logo).attr('src', 'Imagens/Logos/financas_claro_64px.png');
         jQuery(hamburguer).attr('src', 'Imagens/Utilitários/hamburguer_claro_64px.png');
+        jQuery(simboloReceitas).attr('src', 'Imagens/Utilitários/receitas_claro_64px.png');
+        jQuery(simboloDespesas).attr('src', 'Imagens/Utilitários/despesas_claro_64px.png');
+        jQuery(simboloUsuarios).attr('src', 'Imagens/Utilitários/usuarios_claro_64px.png');
+        jQuery(simboloSeguranca).attr('src', 'Imagens/Utilitários/seguranca_claro_64px.png');
+        jQuery(simboloNotificacoes).attr('src', 'Imagens/Utilitários/sino_claro_64px.png');
+        jQuery(simboloPDF).attr('src', 'Imagens/Utilitários/pdf_claro_64px.png');
+    }
+}
+
+// Função para digitar automaticamente no slogan do site
+function autoType(){
+    var digitado = new Typed('.auto-type', {
+        strings: ['gerenciamento', 'confiança', 'economia', 'apoio', 'organização'], // Define as palavras que vão ser digitadas
+        typeSpeed: 150, // Velocidade de digitação
+        backSpeed: 200, // Velocidade que as palavras são apagadas
+        loop: true // Faz tudo rodar infinitamente
+    });
+}
+
+// Função para configurar o grid baseando-se no tamanho da tela
+function configurarGrid(){
+    // Salvando elementos necessários em variáveis
+    var slogan = document.getElementById('sloganContainer');
+    var imagem = document.getElementById('imagemContainer');
+    // Verifica se o tamanho de exibição é menor que 124opx
+    if(window.innerWidth < 1240){
+        // Faz ambas as DIVs ocuparem 100% da tela e estarem uma abaixo da outra
+        slogan.classList.remove('w-50');
+        imagem.classList.remove('w-50');
+        slogan.classList.add('w-100');
+        imagem.classList.add('w-100');
+    }else{
+        // Faz ambas as DIVs ocuparem 50% da tela e estarem lado a lado
+        slogan.classList.remove('w-100');
+        imagem.classList.remove('w-100');
+        slogan.classList.add('w-50');
+        imagem.classList.add('w-50');
+    }
+}
+
+// Função para mudar o fundo da navbar ao rolar a tela, facilitando a visualização
+function navbarVisivel(){
+    // Salva a posição do scroll em um número
+    var scroll = $(window).scrollTop();
+    // Salva o 'body' em uma variável
+    var elemento = document.body;
+    // Se a posição do scroll foi maior que 50
+    if(scroll > 50){
+        // Verifica se está no modo escuro
+        if(elemento.classList.contains('modo-escuro')){
+            // Define a cor como um gradiente com 90% de transparência para o modo escuro
+            $('#navbar').css('background', 'linear-gradient(to right, rgba(37, 41, 140, 0.9), rgba(55, 59, 163, 0.9)');
+        }else{
+            // Define a cor igualmente, mas para o modo claro
+            $('#navbar').css('background', 'linear-gradient(to right, rgba(208, 218, 224, 0.9), rgba(202, 206, 208, 0.9)');
+        }
+    }else{
+        // Volta ao padrão transparente
+        $('#navbar').css('background', 'transparent');
     }
 }
